baffc6deba7c5747d0672fe59bcff9c5607de958
lazysheep <714201376@qq.com>
2018-09-12 17:33:58 +0800

    :kaola: add basic structure

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const webpack = require('webpack')
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        plugins: ['transform-runtime'],
+        presets: ['es2015', 'react', 'stage-2']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
+  }
+}
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+} else {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.HotModuleReplacementPlugin()
+  ])
+  module.exports.devServer = {
+    port: 7777,
+    host: 'localhost',
+    hot: true, // 告诉 dev-server 我们在使用 HMR
+    contentBase: __dirname,
+    inline: true,
+    historyApiFallback: true
+  }
+}
